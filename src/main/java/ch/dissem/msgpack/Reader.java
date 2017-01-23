def99f7a7c1892f00b0f9664ffa1fcc1f4bbb78f
Christian Basler <chrigu.meyer@gmail.com>
2017-01-23 17:38:02 +0100

    Added license (Apache 2.0)

--- src/main/java/ch/dissem/msgpack/Reader.java
+++ src/main/java/ch/dissem/msgpack/Reader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Christian Basler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ch.dissem.msgpack;
 
 import ch.dissem.msgpack.types.*;
@@ -11,7 +27,7 @@ import java.util.List;
  * Reads MPType object from an {@link InputStream}.
  */
 public class Reader {
-    private List<MPType.Unpacker<?>> unpackers = new LinkedList<MPType.Unpacker<?>>();
+    private List<MPType.Unpacker<?>> unpackers = new LinkedList<>();
 
     public Reader() {
         unpackers.add(new MPNil.Unpacker());
